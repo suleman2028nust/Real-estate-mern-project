67f8b5812f0a49a9251e9c0b7caeb82126b97e04
suleman2028nust <sasghar.bscs24seecs@seecs.edu.pk>
2026-07-09 18:29:42 +0500

    deploying

--- api/index.js
+++ api/index.js
@@ -32,8 +32,9 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}!`);
 });
 
 app.use('/api/user', userRouter);
